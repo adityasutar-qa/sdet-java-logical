5ace8ca71c5b947d558b086a66f3415d51f785bc
Aditya Sutar <adityamsutar1@gmail.com>
2025-09-13 12:13:10 +0530

    Logical programs

--- Java_logical/src/logical_programs/Reverse_string.java
+++ Java_logical/src/logical_programs/Reverse_string.java
@@ -2,7 +2,8 @@ package logical_programs;
 
 public class Reverse_string {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		String str= "Automation";
 		String reverse="";
 		for (int i=str.length()-1;i>=0;i--)
